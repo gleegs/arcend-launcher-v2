69fb0061995efcc3748b64432b8e9aa1a18e09e2
Ewixam <ewixam933@gmail.com>
2026-06-19 16:51:19 +0200

    fix(server): fix serveur status

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -4,5 +4,6 @@ export default defineConfig({
   define: {
     'process.env.SUPABASE_URL': JSON.stringify(process.env.SUPABASE_URL),
     'process.env.SUPABASE_PUBLISHABLE_KEY': JSON.stringify(process.env.SUPABASE_PUBLISHABLE_KEY),
+    'process.env.SERVER_HOST': JSON.stringify(process.env.SERVER_HOST),
   },
 })
