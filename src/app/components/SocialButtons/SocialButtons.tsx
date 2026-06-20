cfe6ed9cc6dbc80727adcf7b759035a84955ff3c
Ewixam <ewixam933@gmail.com>
2026-06-20 18:13:10 +0200

    fix(ui): make hidden settings panels non-focusable when closed

--- src/app/components/SocialButtons/SocialButtons.tsx
+++ src/app/components/SocialButtons/SocialButtons.tsx
@@ -9,12 +9,12 @@ export default function SocialButtons() {
   return (
     <div className="gap-3 absolute top-2/5 right-0 p-8 flex flex-col">
       <Button className="p-2">
-        <a href={WEBSITE_URL} target="_blank" rel="noopener noreferrer">
+        <a href={WEBSITE_URL} target="_blank" rel="noopener noreferrer" inert>
           <WebIcon />
         </a>
       </Button>
       <Button className="p-2">
-        <a href={DISCORD_URL} target="_blank" rel="noopener noreferrer">
+        <a href={DISCORD_URL} target="_blank" rel="noopener noreferrer" inert>
           <DiscordIcon />
         </a>
       </Button>
