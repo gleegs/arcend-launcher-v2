fab5150ff8b538a492f26dcc69c6e3e161c3a4fd
ayman-benammour <benammourayman@gmail.com>
2026-06-22 22:35:56 +0200

    style(ui): size only Launcher to fit the sidebar width

--- src/app/components/Sidebar/Sidebar.tsx
+++ src/app/components/Sidebar/Sidebar.tsx
@@ -65,7 +65,10 @@ export default function Sidebar() {
     <div className="bg-white w-20 h-full rounded-2xl px-2 py-4 text-black font-black flex flex-col space-y-4 select-none ">
       <div className="space-y-4">
         <img src={blackLogo} alt="Arcend logo" />
-        <h3 className="uppercase leading-tight text-xs">Arcend Launcher</h3>
+        <h3 className="uppercase leading-tight">
+          Arcend
+          <span className="block text-[13px]">Launcher</span>
+        </h3>
       </div>
       <div className="flex-1 space-y-0.5">
         <h5 className="uppercase font-black text-sm">Arcs</h5>
